281957538a453a6ad17e1ea41f244e0cffbc4099
David <David@David>
2019-02-20 23:34:19 -0500

    agregando metodo nuevolibro

---
+++ Libreria/src/main/java/com/mycompany/libreria/Libros.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.libreria;
+
+/**
+ *
+ * @author David
+ */
+public class Libros {
+    
+    //Atributos
+    
+    String nombreLibro;
+    String autorLibro;
+    String versionLibro;
+    String editorialLibro;
+    int costoLibro;
+    
+    //Metodo
+    public static void main (String[]args){
+       
+        Libros nuevoLibro = new Libros();
+        
+        nuevoLibro.autorLibro = "";
+        nuevoLibro.editorialLibro="";
+        nuevoLibro.costoLibro=0;
+        nuevoLibro.nombreLibro="";
+        nuevoLibro.versionLibro="";
+        
+        
+        
+        
+    }
+
+}
